53e94bf21114480b73ee3ce51e89ba1dcf2df0ce
ashish shrestha <ashishrajshrestha1@gmail.com>
2022-07-12 19:30:41 +0900

    Created a list and new list

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -15,7 +15,7 @@ class ListsController < ApplicationController
   def create
     @list = List.new(list_params)
     if @list.save
-    redirect_to list_path(@list)
+      redirect_to list_path(@list)
     else
       render :new
     end
